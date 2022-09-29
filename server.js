e4de535c296c990318c867be280c1963d13dd79d
Norman <whosnorm@gmail.com>
2022-09-29 15:33:54 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.get(`/greeting/:name`, (req, res)=>{
     
 })
 
+app.get(`/tip/:total/:tipPercentage`, (req,res)=>{
+    const {total, tipPercentage} = req.params
+    const totalTip = ( parseInt(total) * parseInt(tipPercentage)/100)
+    const final = parseInt(total) + parseInt(totalTip)
+    res.send(`Bill: $${total}<br>Tip percentage: ${tipPercentage}% <br>Total tip: $${totalTip} <br> Your final total is: $${final}` )
+})
+
 app.listen(3000, () => {
     console.log(`Server running on port: ${3000}`);
 })
-
