4d4f2ac086937307c389c4f2e70802e759f96465
Norman <whosnorm@gmail.com>
2022-09-29 15:10:29 -0400

    first commit completed

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get(`/greeting/:name`, (req, res)=>{
+   const {name}= req.params
+    res.send(`Hello, ${name} Great to see you!`)
+    
+})
+
+app.listen(3000, () => {
+    console.log(`Server running on port: ${3000}`);
+})
